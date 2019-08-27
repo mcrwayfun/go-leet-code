0e6bb932af01dcc1e1bf32a81ef6eb0f1a412ee8
mcrwayfun <mcrwayfun@gmail.com>
2019-08-27 22:48:17 +0800

    update

--- src/algorithms/089-my-sqrt/my_sqrt_test.go
+++ src/algorithms/089-my-sqrt/my_sqrt_test.go
@@ -11,19 +11,14 @@ func Test_mySqrt(t *testing.T) {
 		args args
 		want int
 	}{
-		//{
-		//	name: "test-case-1",
-		//	args: args{4},
-		//	want: 2,
-		//},
-		//{
-		//	name: "test-case-2",
-		//	args: args{8},
-		//	want: 2,
-		//},
 		{
-			name: "test-case-3",
-			args: args{2147483647},
+			name: "test-case-1",
+			args: args{4},
+			want: 2,
+		},
+		{
+			name: "test-case-2",
+			args: args{8},
 			want: 2,
 		},
 	}
