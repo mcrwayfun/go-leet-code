096adcfc88a036620d429909a193dc726973091c
mcrwayfun <mcrwayfun@gmail.com>
2019-09-03 23:35:06 +0800

    034-search-range

---
+++ src/algorithms/034—search-range/search_range_test.go
@@ -0,0 +1,42 @@
+package _34_search_range
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_searchRange(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test_case_1",
+			args: args{
+				nums:   []int{5, 7, 7, 8, 8, 10},
+				target: 8,
+			},
+			want: []int{3, 4},
+		},
+		{
+			name: "test_case_2",
+			args: args{
+				nums:   []int{5, 7, 7, 8, 8, 10},
+				target: 6,
+			},
+			want: []int{-1, -1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
