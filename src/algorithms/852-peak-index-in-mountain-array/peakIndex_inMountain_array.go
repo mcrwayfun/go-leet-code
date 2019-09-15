1d024ce95e56cf43fee3523618d41e577fceb71d
mcrwayfun <mcrwayfun@gmail.com>
2019-09-15 23:06:23 +0800

    update 852

--- src/algorithms/852-peak-index-in-mountain-array/peakIndex_inMountain_array.go
+++ src/algorithms/852-peak-index-in-mountain-array/peakIndex_inMountain_array.go
@@ -29,3 +29,34 @@ func peakIndexInMountainArray2(A []int) int {
 
 	return left
 }
+
+/**
+我们把符合下列属性的数组 A 称作山脉：
+
+A.length >= 3
+存在 0 < i < A.length - 1 使得A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+给定一个确定为山脉的数组，返回任何满足 A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1] 的 i 的值。
+
+链接：https://leetcode-cn.com/problems/peak-index-in-a-mountain-array
+*/
+func peakIndexInMountainArray3(A []int) int {
+	if len(A) < 3 {
+		return -1
+	}
+
+	start, end := 0, len(A)-1
+	for start+1 < end {
+		mid := start + (end-start)/2
+		if A[mid] < A[mid+1] {
+			start = mid
+		} else {
+			end = mid
+		}
+	}
+
+	if A[start] > A[end] {
+		return start
+	} else {
+		return end
+	}
+}
--- src/algorithms/852-peak-index-in-mountain-array/peakIndex_inMountain_array_test.go
+++ src/algorithms/852-peak-index-in-mountain-array/peakIndex_inMountain_array_test.go
@@ -69,3 +69,37 @@ func Test_peakIndexInMountainArray2(t *testing.T) {
 		})
 	}
 }
+
+func Test_peakIndexInMountainArray3(t *testing.T) {
+	type args struct {
+		A []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test-case1",
+			args: args{[]int{0, 1, 0}},
+			want: 1,
+		},
+		{
+			name: "test-case2",
+			args: args{[]int{0, 2, 1, 0}},
+			want: 1,
+		},
+		{
+			name: "test-case2",
+			args: args{[]int{0, 2, 3, 4}},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := peakIndexInMountainArray3(tt.args.A); got != tt.want {
+				t.Errorf("peakIndexInMountainArray3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
