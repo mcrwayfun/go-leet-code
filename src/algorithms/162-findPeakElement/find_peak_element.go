2aeff02b421e76dfd5d5cedfd9465d94db949414
mcrwayfun <mcrwayfun@gmail.com>
2019-09-15 23:25:47 +0800

    162-findPeakElement

---
+++ src/algorithms/162-findPeakElement/find_peak_element.go
@@ -0,0 +1,32 @@
+package _62_findPeakElement
+
+/**
+峰值元素是指其值大于左右相邻值的元素。
+
+给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+
+数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+
+你可以假设 nums[-1] = nums[n] = -∞。
+
+链接：https://leetcode-cn.com/problems/find-peak-element
+time complexity:O(lgn)
+space complexity:O(1)
+*/
+func findPeakElement(nums []int) int {
+	start, end := 0, len(nums)-1
+	for start+1 < end {
+		mid := start + (end-start)/2
+		if nums[mid] < nums[mid+1] {
+			start = mid
+		} else {
+			end = mid
+		}
+	}
+
+	if nums[start] > nums[end] {
+		return start
+	} else {
+		return end
+	}
+}
