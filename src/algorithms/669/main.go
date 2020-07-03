818efbfad0d4b5f7f82178900c10a669742f509e
mcrwayfun <mcrwayfun@gmail.com>
2020-07-03 23:53:01 +0800

    669

---
+++ src/algorithms/669/main.go
@@ -0,0 +1,55 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func int2TreeNode(ints []int) *TreeNode {
+	n := len(ints)
+	if n == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: ints[0],
+	}
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+		if i < n && ints[i] != -1 {
+			node.Left = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+		if i < n && ints[i] != -1 {
+			node.Right = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+	return root
+}
+
+/// Time Complexity: O(n)
+/// Space Complexity: O(h)
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val < L {// 根节点小于L,则说明左子树应全部被剪枝
+		return trimBST(root.Right, L, R)
+	}
+
+	if root.Val > R {// 根节点小于R,则说明左子树应全部被剪枝
+		return trimBST(root.Left, L, R)
+	}
+
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
