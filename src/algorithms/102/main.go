f08e885864a61a1bcd393323d36cc159073c99b9
mcrwayfun <mcrwayfun@gmail.com>
2020-07-03 13:09:45 +0800

    102

---
+++ src/algorithms/102/main.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func int2TreeNode(ints []int) *TreeNode {
+	n := len(ints)
+	if n == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: ints[0],
+	}
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+		if i < n && ints[i] != 0 {
+			node.Left = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+		if i < n && ints[i] != 0 {
+			node.Right = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+	return root
+}
+
+/// Time Complexity: O(n)
+/// Space Complexity: O(h)
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	var s [][]int
+	bfs(root, &s, 0)
+	return s
+}
+
+func bfs(root *TreeNode, s *[][]int, level int) {
+	if root == nil {
+		return
+	}
+
+	if len(*s) == level {
+		*s = append(*s, []int{})
+	}
+
+	(*s)[level] = append((*s)[level], root.Val)
+	for _, v := range []*TreeNode{root.Left, root.Right} {
+		bfs(v, s, level+1)
+	}
+	return
+}
+
+func main() {
+	node := []int{3, 9, 20, 0, 0, 15, 7}
+	root := int2TreeNode(node)
+	order := levelOrder(root)
+	fmt.Println(order)
+}
