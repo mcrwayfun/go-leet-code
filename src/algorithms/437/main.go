658cce92335f5c2329099fadee32602c907d79f3
mcrwayfun <mcrwayfun@gmail.com>
2020-07-04 12:01:27 +0800

    124

---
+++ src/algorithms/437/main.go
@@ -0,0 +1,64 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func int2TreeNode(ints []int) *TreeNode {
+	n := len(ints)
+	if n == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: ints[0],
+	}
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+		if i < n && ints[i] != -1 {
+			node.Left = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+		if i < n && ints[i] != -1 {
+			node.Right = &TreeNode{Val: ints[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+	return root
+}
+
+/// Time Complexity: O(n)
+/// Space Complexity: O(h)
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	return dfs(root, sum) +
+		pathSum(root.Left, sum) +
+		pathSum(root.Right, sum)
+}
+
+func dfs(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	var res int
+	if root.Val == sum {
+		res += 1
+	}
+
+	for _, v := range []*TreeNode{root.Left, root.Right}{
+		res += dfs(v, sum - root.Val)
+	}
+
+	return res
+}
