6d730f58e1a447e6a4bb9752b0cd09a5502ef30e
mcrwayfun <mcrwayfun@gmail.com>
2020-07-09 12:53:43 +0800

    34

--- src/algorithms/34/main.go
+++ src/algorithms/34/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 /// Time Complexity: O(n)
-/// Space Complexity: O(h)
+/// Space Complexity: O(1)
 func searchRange(nums []int, target int) []int {
 	left := 0
 	right := len(nums) - 1
@@ -31,8 +31,56 @@ func searchRange(nums []int, target int) []int {
 	return []int{-1, -1}
 }
 
+/// Time Complexity: O(n)
+/// Space Complexity: O(1)
+func searchRange2(nums []int, target int) []int {
+	return []int{searchFirstEqualElement(nums, target), searchLastEqualElement(nums, target)}
+}
+
+func searchFirstEqualElement(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else { // nums[mid] == target
+			if mid == 0 || nums[mid-1] != target {
+				return mid
+			}
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func searchLastEqualElement(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else { // nums[mid] == target
+			if mid == len(nums)-1 || nums[mid+1] != target {
+				return mid
+			}
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	ints := []int{5, 7, 7, 8, 8, 10}
-	ret := searchRange(ints, 8)
+	ret := searchRange2(ints, 8)
 	fmt.Println(ret)
 }
