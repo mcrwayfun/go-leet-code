114ce081fb8e571668c58ca294440afc9700eb97
mcrwayfun <mcrwayfun@gmail.com>
2021-07-11 18:30:56 +0800

    120_三角形最小路径和

--- 120_三角形最小路径和/main.go
+++ 120_三角形最小路径和/main.go
@@ -57,6 +57,43 @@ func divideConquer(triangle [][]int, x, y int, hash [][]int) int {
 	return hash[x][y]
 }
 
+// 动态规划算法（从上往下）
+// time complexity: O(n^2)
+// space complexity: O(n^2)
+func minimumTotalV3(triangle [][]int) int {
+	if len(triangle) == 0 {
+		return -1
+	}
+	if len(triangle[0]) == 0 {
+		return -1
+	}
+
+	sum := initHash(triangle, 0)
+	m := len(triangle)
+	sum[0][0] = triangle[0][0]
+
+	// 初始化第一列和斜着的
+	for i := 1; i < m; i++ {
+		sum[i][0] = sum[i-1][0] + triangle[i][0]
+		sum[i][i] = sum[i-1][i-1] + triangle[i][i]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < i; j++ { // 只用了三角形的左下半
+			sum[i][j] = min(sum[i-1][j], sum[i-1][j-1]) + triangle[i][j]
+		}
+	}
+
+	// fmt.Println(sum)
+	// [[2] [5 6] [11 10 13] [15 11 18 16]]
+	// 获取从[0,0]到最底层的最短路径, 只统计最后一行
+	pathSum := sum[m-1][0]
+	for i := 1; i < len(sum[m-1]); i++ {
+		pathSum = min(pathSum, sum[m-1][i])
+	}
+	return pathSum
+}
+
 func min(a, b int) int {
 	if a > b {
 		return b
@@ -87,5 +124,9 @@ func main() {
 
 	nums = [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
 	result = minimumTotalV2(nums)
+	fmt.Println(result)
+
+	nums = [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
+	result = minimumTotalV3(nums)
 	fmt.Println(result)
 }
