bbb2cb38a1ad4c8f21f7dab1c30422d03c0c06b4
mcrwayfun <mcrwayfun@gmail.com>
2022-02-27 10:41:29 +0800

    review first

---
+++ review/0227/01/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func findMinSubArray(s int, arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+
+	var windowStart int
+	var windowSum int
+	var minLength = len(arr) + 1
+
+	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
+		windowSum += arr[windowEnd]
+		for windowSum >= s {
+			minLength = min(minLength, windowEnd-windowStart+1)
+			windowSum -= arr[windowStart]
+			windowStart++
+		}
+	}
+
+	if minLength == len(arr)+1 {
+		return 0
+	}
+
+	return minLength
+}
+
+func main() {
+	arr := []int{2, 1, 5, 2, 3, 2}
+	k := 7
+
+	arr2 := []int{2, 1, 5, 2, 8}
+	k2 := 7
+
+	fmt.Println(findMinSubArray(k, arr))
+	fmt.Println(findMinSubArray(k2, arr2))
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
