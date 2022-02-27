7b48d7ac1d86d8602b3cea37c4eb9577d55606d4
mcrwayfun <mcrwayfun@gmail.com>
2022-02-27 15:07:40 +0800

    06_no_repeat_substring

--- grokking-the-coding-interview/02-sliding-window/06_no_repeat_substring/main.go
+++ grokking-the-coding-interview/02-sliding-window/06_no_repeat_substring/main.go
@@ -46,11 +46,47 @@ func findLength(str string) int {
 		var charFrequencyMap = make(map[byte]int, 0)
 		for j := i; j < len(str); j++ {
 			if _, ok := charFrequencyMap[str[j]]; ok {
-				maxLength = max(maxLength, j-i+1-1)
+				// 以下两种方式均可以
+				// maxLength = max(maxLength, j-i+1-1)
+				// maxLength = max(maxLength, len(charFrequencyMap))
+				maxLength = max(maxLength, j-i)
 				break
 			}
 			charFrequencyMap[str[j]]++
 		}
+	}
+
+	return maxLength
+}
+
+/**
+思路2：滑动窗口法
+1：使用全局变量maxLength来记录符合条件的子串的最大长度，使用map来记录字符出现的下标。
+使用windowStart来记录窗口的起始位置
+2：遍历字符串str，
+	2-1：如果字符在map中存在的话，需要滑动窗口。此时将 windowStart = 该字符上次出现的位置 + 1
+	2-2：如果字符在map中没有出现过的话，则 map[key] = windowEnd
+	2-3：maxLength = windowEnd - windowStart + 1（此时是符合条件的，子串中没有重复的）
+
+time complexity: O(n)
+space complexity: O(1)
+*/
+func findLength2(str string) int {
+	if len(str) == 0 {
+		return 0
+	}
+
+	var windowStart int
+	var charIndexMap = make(map[byte]int, len(str))
+	var maxLength int
+
+	for windowEnd := 0; windowEnd < len(str); windowEnd++ {
+		char := str[windowEnd]
+		if _, ok := charIndexMap[char]; ok {
+			windowStart = charIndexMap[char] + 1 // need to shrink window
+		}
+		charIndexMap[char] = windowEnd
+		maxLength = max(maxLength, windowEnd-windowStart+1)
 	}
 
 	return maxLength
@@ -63,6 +99,10 @@ func main() {
 	fmt.Println(findLength(str))
 	fmt.Println(findLength(str2))
 	fmt.Println(findLength(str3))
+
+	fmt.Println(findLength2(str))
+	fmt.Println(findLength2(str2))
+	fmt.Println(findLength2(str3))
 }
 
 func max(a, b int) int {
