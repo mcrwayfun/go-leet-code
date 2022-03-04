3aa63fb1ec855002ec191dcf792fb01c3100940f
mcrwayfun <mcrwayfun@gmail.com>
2022-03-04 23:44:25 +0800

    0304-05

---
+++ review/0304/05/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func findLength(str string) int {
+	if len(str) == 0 {
+		return 0
+	}
+
+	var windowStart int
+	var maxLength int
+	var charIndexMap = make(map[byte]int, len(str))
+
+	// aabccbb -> abc -> 3
+	for windowEnd := 0; windowEnd < len(str); windowEnd++ {
+		char := str[windowEnd]
+		if _, ok := charIndexMap[char]; ok {
+			windowStart = charIndexMap[char] + 1
+		}
+		fmt.Println("windowEnd - windowStart + 1", windowEnd-windowStart+1)
+		charIndexMap[char] = windowEnd
+		maxLength = max(maxLength, windowEnd-windowStart+1)
+	}
+
+	return maxLength
+}
+
+func main() {
+	str := "aabccbb"
+	str2 := "abbbb"
+	str3 := "abccde"
+	fmt.Println(findLength(str))
+	fmt.Println(findLength(str2))
+	fmt.Println(findLength(str3))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
