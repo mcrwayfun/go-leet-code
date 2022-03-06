29181ba658885d796bd5c3fd1753da881c2713fd
mcrwayfun <mcrwayfun@gmail.com>
2022-03-06 11:57:03 +0800

    update comment

--- grokking-the-coding-interview/02-sliding-window/07_longest_substring_with_same_letters_after_replacement/main.go
+++ grokking-the-coding-interview/02-sliding-window/07_longest_substring_with_same_letters_after_replacement/main.go
@@ -32,7 +32,7 @@ func findLength(str string, k int) int
 
 /**
 解题思路：使用滑动窗口的思想，这里的关键是 滑动窗口的时机
-1：使用map来统计元素出现的次数，维护一个 maxRepeatCount 来统计当前窗口中同个元素出现的最大个数。
+1：使用map来统计元素出现的次数，维护一个 maxRepeatCount 来统计任意窗口（记住这里不是当前窗口）中同个元素出现的最大个数。
 2：滑动窗口的时机：windowEnd-windowStart+1-maxRepeatCount > k，举个例子：对于aabccbb，k=2
 假设当前窗口为 aabcc, 此时：
 	windowEnd=4, windowStart=0, windowSize=5, maxRepeatCount=2(aa/cc), k=2
@@ -54,7 +54,6 @@ func findLength(str string, k int) int {
 	for windowEnd := 0; windowEnd < len(str); windowEnd++ {
 		charFrequencyMap[str[windowEnd]]++
 		maxRepeatCount = max(maxRepeatCount, charFrequencyMap[str[windowEnd]])
-
 		if windowEnd-windowStart+1-maxRepeatCount > k {
 			if _, ok := charFrequencyMap[str[windowStart]]; ok {
 				charFrequencyMap[str[windowStart]]--
@@ -72,7 +71,9 @@ func main() {
 	str := "aabccbb"
 	k := 2
 
+	str2 := "aaaabbbaaacd"
 	fmt.Println(findLength(str, k))
+	fmt.Println(findLength(str2, k))
 }
 
 func max(a, b int) int {
