d4979c0b5fb4563285073adf4265a12d46559bef
mcrwayfun <mcrwayfun@gmail.com>
2022-03-08 20:35:53 +0800

    review/0308-01

---
+++ review/0308/01/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func findStringAnagrams(str string, pattern string) []int {
+	if len(str) == 0 {
+		return []int{}
+	}
+
+	var charFrequencyMap = make(map[byte]int, len(pattern))
+	for i := 0; i < len(pattern); i++ {
+		charFrequencyMap[pattern[i]]++
+	}
+
+	var windowStart int
+	var matched int
+	var startIndices []int
+
+	for windowEnd := 0; windowEnd < len(str); windowEnd++ {
+		rightChar := str[windowEnd]
+		if _, ok := charFrequencyMap[rightChar]; ok {
+			charFrequencyMap[rightChar]--
+			if charFrequencyMap[rightChar] == 0 {
+				matched++
+			}
+		}
+
+		if len(pattern) == matched {
+			startIndices = append(startIndices, windowStart)
+		}
+
+		if windowEnd-windowStart+1 >= len(pattern) {
+			leftChar := str[windowStart]
+			if _, ok := charFrequencyMap[leftChar]; ok {
+				if charFrequencyMap[leftChar] == 0 {
+					matched--
+				}
+				charFrequencyMap[leftChar]++
+			}
+			windowStart++
+		}
+	}
+
+	return startIndices
+}
+
+func main() {
+	str := "ppqp"
+	pattern := "pq"
+
+	str2 := "abbcabc"
+	pattern2 := "abc"
+
+	fmt.Println(findStringAnagrams(str, pattern))
+	fmt.Println(findStringAnagrams(str2, pattern2))
+}
