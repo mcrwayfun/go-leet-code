be4998d61a8e8e55143b919b23d7ad5a041446b1
mcrwayfun <mcrwayfun@gmail.com>
2022-03-13 11:52:40 +0800

    review/0311-03

---
+++ review/0313/03/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func findLength(arr []int, k int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+
+	var windowStart int
+	var numOnesCount int
+	var maxLength int
+
+	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
+		rightNum := arr[windowEnd]
+		if rightNum == 1 {
+			numOnesCount++
+		}
+
+		if windowEnd-windowStart+1-numOnesCount > k {
+			leftNum := arr[windowStart]
+			if leftNum == 1 {
+				numOnesCount--
+			}
+			windowStart++
+		}
+
+		maxLength = max(maxLength, windowEnd-windowStart+1)
+	}
+
+	return maxLength
+}
+
+func main() {
+	arr := []int{0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1}
+	k := 2
+
+	arr2 := []int{0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1}
+	k2 := 3
+
+	fmt.Println(findLength(arr, k))
+	fmt.Println(findLength(arr2, k2))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
