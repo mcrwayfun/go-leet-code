600bb6594272c64a59318aa17c8205226c2f592a
mcrssuper <mcrwayfun@gmail.com>
2022-05-05 19:23:20 +0800

    0505-07

---
+++ review/0505/06/main.go
@@ -0,0 +1,21 @@
+package main
+
+func twoSum(numbers []int, target int) []int {
+	if len(numbers) == 0 {
+		return []int{-1, -1}
+	}
+
+	var start int
+	var end = len(numbers) - 1
+	for start < end {
+		num := numbers[start] + numbers[end]
+		if num == target {
+			return []int{start + 1, end + 1}
+		} else if num < target {
+			start++
+		} else {
+			end--
+		}
+	}
+	return []int{-1, -1}
+}
