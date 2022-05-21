c89adaa48e4892bd1acaf5a40f0485c892c86399
mcrwayfun <mcrwayfun@gmail.com>
2022-05-21 23:55:30 +0800

    feat: solution for leet-code-155
    write the solution for leetcode 155: the min stack

--- leet-code/155/main.go
+++ leet-code/155/main.go
@@ -5,35 +5,105 @@ import (
 	"math"
 )
 
+/**
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+
+
+示例 1:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+模板：
 type MinStack struct {
-	stack    []int
-	minStack []int
+
 }
 
-/** initialize your data structure here. */
+
+func Constructor() MinStack {
+
+}
+
+
+func (this *MinStack) Push(val int)  {
+
+}
+
+
+func (this *MinStack) Pop()  {
+
+}
+
+
+func (this *MinStack) Top() int {
+
+}
+
+
+func (this *MinStack) GetMin() int {
+
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+*/
+
+type MinStack struct {
+	stack []int
+	min   []int
+}
+
 func Constructor() MinStack {
 	return MinStack{
-		stack:    []int{},
-		minStack: []int{math.MaxInt64}, // maxInt
+		stack: []int{},
+		min:   []int{math.MaxInt64},
 	}
 }
 
-func (this *MinStack) Push(x int) {
-	this.stack = append(this.stack, x)
-	top := this.minStack[len(this.minStack)-1]
-	this.minStack = append(this.minStack, min(top, x))
-}
-
-func min(a, b int) int {
-	if a > b {
-		return b
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.min) == 0 || this.GetMin() >= val {
+		this.min = append(this.min, val)
 	}
-	return a
 }
 
 func (this *MinStack) Pop() {
+	if this.Top() == this.GetMin() {
+		this.min = this.min[:len(this.min)-1]
+	}
 	this.stack = this.stack[:len(this.stack)-1]
-	this.minStack = this.minStack[:len(this.minStack)-1]
 }
 
 func (this *MinStack) Top() int {
@@ -41,8 +111,17 @@ func (this *MinStack) Top() int {
 }
 
 func (this *MinStack) GetMin() int {
-	return this.minStack[len(this.minStack)-1]
+	return this.min[len(this.min)-1]
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
 
 /**
  * Your MinStack object will be instantiated and called as such:
