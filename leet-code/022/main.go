3e495e47dcb5b08b85500e345fc5fd53180eb646
mcrssuper <mcrwayfun@gmail.com>
2022-06-02 09:39:18 +0800

    feat: solution of leetcode-22. Generate Parentheses

--- leet-code/022/main.go
+++ leet-code/022/main.go
@@ -2,41 +2,62 @@ package main
 
 import "fmt"
 
+/**
+数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+示例 1：
+输入：n = 3
+输出：["((()))","(()())","(())()","()(())","()()()"]
+
+示例 2：
+输入：n = 1
+输出：["()"]
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/generate-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+模板：
+func generateParenthesis(n int) []string {}
+ */
+
+/**
+形成有效的括号组合，什么是有效的？前提是小于n
+对于(, 任何情况下都是有效的
+对于)，只要有一个(, )就是有效的
+
+所以有规则，假设left和right分别表示可以生成的左右括号的次数
+只要有 left>0, 就可以添加左括号
+只要有 right>left, 就可以添加右括号
+
+time complexity: O(n!)
+space complexity: O(1)
+ */
 func generateParenthesis(n int) []string {
-	var ans []string
-	if n == 0 {
-		return ans
+	if n <= 0 {
+		return []string{}
 	}
 
-	var track string
-	backtrack(n, n, track, &ans)
-	return ans
+	var res []string
+	generate(&res, "", n, n)
+	return res
 }
 
-func backtrack(left, right int, track string, ans *[]string) {
-	if right < left {
-		return
-	}
-
-	if left < 0 || right < 0 {
-		return
-	}
-
+func generate(res *[]string, str string, left int, right int) {
 	if left == 0 && right == 0 {
-		*ans = append(*ans, track)
-		return
+		*res = append(*res, str)
+	} else {
+		if left > 0 {
+			generate(res, str+"(", left-1, right)
+		}
+		if right > left {
+			generate(res, str+")", left, right-1)
+		}
 	}
-
-	track = track + "("
-	backtrack(left-1, right, track, ans)
-	track = track[:len(track)-1]
-
-	track = track + ")"
-	backtrack(left, right-1, track, ans)
-	track = track[:len(track)-1]
 }
 
 func main() {
-	parenthesisa := generateParenthesis(3)
-	fmt.Println(parenthesisa)
+	fmt.Println(generateParenthesis(3))
 }
+
+
