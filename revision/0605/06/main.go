0b29839c9d1708b8dd775e419232d4fdc2d7e246
mcrssuper <mcrwayfun@gmail.com>
2022-06-05 13:21:42 +0800

    feat: solution of leetcode-88. Merge Sorted Array

---
+++ revision/0605/06/main.go
@@ -0,0 +1,25 @@
+package main
+
+// time complexity: O(m+n)
+// space complexity: O(1)
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+	var k = m + n - 1
+	var i = m - 1
+	var j = n - 1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+}
