5ea480a43c932561c16e5a49d397699370611a7f
mcrssuper <mcrwayfun@gmail.com>
2022-06-23 08:38:00 +0800

    feat: solution of leetcode-46. Permutations

---
+++ revision/0623/03/main.go
@@ -0,0 +1,23 @@
+package main
+
+func permute(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+
+	var res [][]int
+	helper(nums, 0, &res)
+	return res
+}
+
+func helper(nums []int, start int, res *[][]int) {
+	if start == len(nums) {
+		*res = append(*res, append([]int{}, nums...))
+	} else {
+		for i:=start; i<len(nums); i++ {
+			nums[i], nums[start] = nums[start], nums[i]
+			helper(nums, start+1, res)
+			nums[i], nums[start] = nums[start], nums[i]
+		}
+	}
+}
