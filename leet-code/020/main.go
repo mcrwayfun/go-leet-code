1aec271c4a47cfc6df704c63f489011a235c388c
mcrssuper <mcrwayfun@gmail.com>
2022-06-26 12:33:17 +0800

    feat: solution of leetcode-20. Valid Parentheses

--- leet-code/020/main.go
+++ leet-code/020/main.go
@@ -1,28 +1,61 @@
 package main
 
+/**
+给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+
+示例 1：
+输入：s = "()"
+输出：true
+
+示例2：
+输入：s = "()[]{}"
+输出：true
+
+示例3：
+输入：s = "(]"
+输出：false
+
+示例4：
+输入：s = "([)]"
+输出：false
+
+示例5：
+输入：s = "{[]}"
+输出：true
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func isValid(s string) bool {}
+*/
+
+// time complexity: O(n)
+// space complexity: O(n)
 func isValid(s string) bool {
-	// 这道题空串也是有效的...
-	if len(s) % 2 != 0 {
-		return false
-	}
-
-	symbol := map[byte]byte {
-		')':'(',
-		'}':'{',
-		']':'[',
-	}
-
 	var stack []byte
-	for i:=0;i<len(s);i++ {
-		if v, ok := symbol[s[i]]; ok {
-			if len(stack) == 0 || stack[len(stack)-1] != v {
-				return false // 括号不匹配
-			}
-			stack = stack[:len(stack)-1]
-		} else {
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
 			stack = append(stack, s[i])
+		} else if len(stack) == 0 {
+			return false
+		} else {
+			right := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if s[i] == ')' && right != '(' {
+				return false
+			}
+			if s[i] == ']' && right != '[' {
+				return false
+			}
+			if s[i] == '}' && right != '{' {
+				return false
+			}
 		}
 	}
-
 	return len(stack) == 0
 }
