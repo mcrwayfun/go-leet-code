a9c2a00c2e6f46a4d5c146993427f2de279ad692
mcrssuper <mcrwayfun@gmail.com>
2022-06-28 09:49:56 +0800

    feat: solution of leetcode-98. Validate Binary Search Tree

---
+++ revision/0628/01/main.go
@@ -0,0 +1,36 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return isValidBSTBound(root, nil, nil)
+}
+
+func isValidBSTBound(root *TreeNode, lower, upper *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	if lower != nil && root.Val <= lower.Val {
+		return false
+	}
+
+	if upper != nil && root.Val >= upper.Val {
+		return false
+	}
+
+	return isValidBSTBound(root.Left, lower, root) &&
+		isValidBSTBound(root.Right, root, upper)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
