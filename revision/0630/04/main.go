f9bf9d7c41adfb0aacabae3db3e85b357ffaf66f
mcrssuper <mcrwayfun@gmail.com>
2022-06-30 08:20:36 +0800

    feat: 121. Best Time to Buy and Sell Stock

---
+++ revision/0630/04/main.go
@@ -0,0 +1,25 @@
+package main
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	var buy = prices[0] // 当前购买股票的最低价
+	var maxProfit int // 最大利润
+	for i:=1; i<len(prices); i++ {
+		if prices[i] < buy {
+			buy = prices[i]
+		} else {
+			maxProfit = max(maxProfit, prices[i]-buy)
+		}
+	}
+	return maxProfit
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
