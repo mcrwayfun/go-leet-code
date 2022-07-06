c88a7fc074165a028e472780dab977600e9edce2
mcrssuper <mcrwayfun@gmail.com>
2022-07-07 07:36:17 +0800

    feat: 64. Minimum Path Sum

--- leet-code/064/main.go
+++ leet-code/064/main.go
@@ -2,40 +2,63 @@ package main
 
 import "fmt"
 
-// time complexity: O(n^2)
-// space complexity: O(n^2)
+/**
+给定一个包含非负整数的 mxn网格grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。
+
+
+示例 1：
+输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+输出：7
+解释：因为路径 1→3→1→1→1 的总和最小。
+示例 2：
+
+输入：grid = [[1,2,3],[4,5,6]]
+输出：12
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/minimum-path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+模板：func minPathSum(grid [][]int) int {}
+*/
+
+/**
+解题思路：
+假设存储最优解的数据为d，那么当前坐标的最优解为d(i,j), 结果最优解为d(m-1,n-1)
+1：对于首行，只能向右移动，有：d(0,j)=d(0,j-1)+a(0,j)
+2：对于首列，只能向下移动，有：d(i,0)=d(i-1,0)+a(i,0)
+3：对于其他元素，值可能来自于上左，则有：d(i,j)=min(d(i-1,j), d(i,j-1)) + a(i,j)
+
+time complexity: O(m*n)
+space complexity: O(m*n)
+*/
 func minPathSum(grid [][]int) int {
-	if len(grid) == 0 {
-		return -1
+	var m = len(grid)
+	var n = len(grid[0])
+
+	var d = make([][]int, m)
+	for i := 0; i < m; i++ {
+		d[i] = make([]int, n)
 	}
 
-	if len(grid[0]) == 0 {
-		return -1
-	}
-
-	m := len(grid)
-	n := len(grid[0])
-
-	// init sum
-	sum := initSum(m, n)
-	sum[0][0] = grid[0][0]
-
-	// init sum first row
+	d[0][0] = grid[0][0]
 	for i := 1; i < m; i++ {
-		sum[i][0] = sum[i-1][0] + grid[i][0]
+		d[i][0] = d[i-1][0] + grid[i][0]
 	}
-	// init sum first column
-	for i := 1; i < n; i++ {
-		sum[0][i] = sum[0][i-1] + grid[0][i]
+
+	for j := 1; j < n; j++ {
+		d[0][j] = d[0][j-1] + grid[0][j]
 	}
 
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
-			sum[i][j] = min(sum[i-1][j], sum[i][j-1]) + grid[i][j]
+			d[i][j] = min(d[i-1][j], d[i][j-1]) + grid[i][j]
 		}
 	}
 
-	return sum[m-1][n-1]
+	return d[m-1][n-1]
 }
 
 func min(a, b int) int {
@@ -43,14 +66,6 @@ func min(a, b int) int {
 		return b
 	}
 	return a
-}
-
-func initSum(m, n int) [][]int {
-	sum := make([][]int, m)
-	for i := range sum {
-		sum[i] = make([]int, n)
-	}
-	return sum
 }
 
 func main() {
