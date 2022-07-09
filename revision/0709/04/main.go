18ca1242a8c6a0ed1cd41741c20dc46aee0639e0
mcrssuper <mcrwayfun@gmail.com>
2022-07-09 18:00:36 +0800

    feat: 64. Minimum Path Sum

---
+++ revision/0709/04/main.go
@@ -0,0 +1,29 @@
+package majn
+
+// time complexity: O(n^2)
+// space complexity: O(n)
+func minPathSum(grid [][]int) int {
+	var m = len(grid)
+	var n = len(grid[0])
+
+	var d = make([]int, n)
+	d[0] = grid[0][0]
+	for j := 1; j < n; j++ {
+		d[j] = d[j-1] + grid[0][j]
+	}
+
+	for i := 1; i < m; i++ {
+		d[0] += grid[i][0]
+		for j := 1; j < n; j++ {
+			d[j] = min(d[j], d[j-1]) + grid[i][j]
+		}
+	}
+	return d[n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
