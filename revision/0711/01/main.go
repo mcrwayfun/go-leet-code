dac30602ac86598df5cc8dfb8279b7b9128f7271
mcrssuper <mcrwayfun@gmail.com>
2022-07-11 08:42:58 +0800

    feat: 28. Implement strStr()

---
+++ revision/0711/01/main.go
@@ -0,0 +1,23 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return -1
+	}
+
+	var n = len(haystack)
+	var m = len(needle)
+	for i:=0; i<=n-m; i++ {
+		var j = 0
+		var z = i
+		for j<m && haystack[z] == needle[j] {
+			j++
+			z++
+		}
+
+		if j == len(needle) {
+			return i
+		}
+	}
+	return -1
+}
