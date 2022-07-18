fc32b8c04e606b32331eea10cdc1cd4843216b7a
mcrssuper <mcrwayfun@gmail.com>
2022-07-19 07:52:11 +0800

    feat: 121. Best Time to Buy and Sell Stock

---
+++ revision/0719/main.go
@@ -0,0 +1,28 @@
+package main
+
+// time complexity: O(n)
+// space complexity: O(1)
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	var maxProfit int
+	var buy = prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < buy {
+			buy = prices[i]
+		} else {
+			maxProfit = max(maxProfit, prices[i]-buy)
+		}
+	}
+
+	return maxProfit
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
