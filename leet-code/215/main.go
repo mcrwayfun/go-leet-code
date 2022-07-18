5f4a1010cff7970f9d387f5be201095856f47d06
mcrssuper <mcrwayfun@gmail.com>
2022-07-18 23:02:10 +0800

    feat: 215. Kth Largest Element in an Array

--- leet-code/215/main.go
+++ leet-code/215/main.go
@@ -2,81 +2,75 @@ package main
 
 import "fmt"
 
+/**
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+示例 1:
+
+输入: [3,2,1,5,6,4], k = 2
+输出: 5
+示例2:
+
+输入: [3,2,3,1,2,4,5,5,6], k = 4
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/kth-largest-element-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+模板：func findKthLargest(nums []int, k int) int {}
+*/
+
+/**
+使用快速选择法来解答，根据题意，需要降序排序，求第k大的，等同于求数组中下标为k-1的元素，快速选择法的逻辑如下：
+1：选择一个基值pivot，定义两个指针i和j，默认选择指针i对应的元素作为基准值
+2：for nums[j] < pivot then j--
+3：swap nums[i] and nums[j]
+4：for nums[i] >= pivot then i++
+5：swap nums[i] and nums[j]
+最后得到一个降序的数组
+
+当原数组是升序的，最坏的时间复杂度为 O(n^2)，可以使用洗牌算法打乱数组，使得时间复杂度降到O(n)
+平均时间复杂度为 O(n)
+*/
 func findKthLargest(nums []int, k int) int {
-	nums = quickSort(nums)
-	return nums[len(nums)-k]
-}
+	var low = 0
+	var high = len(nums) - 1
 
-func mergeSort(arr []int) []int {
-	length := len(arr)
-	if length < 2 {
-		return arr
-	}
-	mid := length / 2
-	return merge(mergeSort(arr[:mid]), mergeSort(arr[mid:]))
-}
-
-func merge(left []int, right []int) []int {
-	var ret []int
-	for len(left) != 0 && len(right) != 0 {
-		if left[0] <= right[0] {
-			ret = append(ret, left[0])
-			left = left[1:]
+	for low <= high {
+		p := partition(nums, low, high)
+		if p == k-1 {
+			return nums[p]
+		} else if p < k-1 {
+			low = p + 1
 		} else {
-			ret = append(ret, right[0])
-			right = right[1:]
+			high = p - 1
 		}
 	}
 
-	if len(left) != 0 {
-		ret = append(ret, left...)
-	}
-
-	if len(right) != 0 {
-		ret = append(ret, right...)
-	}
-	return ret
+	return -1
 }
 
-func quickSort(arr []int) []int {
-	return _quickSort(arr, 0, len(arr)-1)
-}
+func partition(nums []int, low, high int) int {
+	var pivot = nums[low]
+	var i = low
+	var j = high
 
-func _quickSort(arr []int, left, right int) []int {
-	if left >= right {
-		return arr
+	for i < j {
+		for ; i < j && nums[j] < pivot; j-- {
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		for ; i < j && nums[i] >= pivot; i++ {
+		}
+		nums[i], nums[j] = nums[j], nums[i]
 	}
-
-	index := sort(arr, left, right)
-	_quickSort(arr, left, index-1)
-	_quickSort(arr, index+1, right)
-	return arr
-}
-
-func sort(arr []int, left, right int) int {
-	i, j := left, right
-	index := left // 基准
-
-	for i != j {
-		for arr[j] >= arr[index] && i < j {
-			j--
-		}
-
-		for arr[i] <= arr[index] && i < j {
-			i++
-		}
-
-		if i < j {
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-	}
-
-	arr[index], arr[i] = arr[i], arr[index]
-	return i
+	return i // i == j
 }
 
 func main() {
-	num := []int{1,5,2,4,3}
-	ans := findKthLargest(num, 5)
+	num := []int{3, 2, 1, 5, 6, 4}
+	ans := findKthLargest(num, 2)
 	fmt.Println(ans)
 }
