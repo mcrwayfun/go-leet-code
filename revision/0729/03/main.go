7582a7053733fe230e0b23b60b115c5664bb4927
mcrssuper <mcrwayfun@gmail.com>
2022-07-29 23:43:10 +0800

    feat: 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ revision/0729/03/main.go
@@ -0,0 +1,33 @@
+package main
+
+// 先序遍历：中左右
+// 中序遍历：左中右
+// time complexity: O(n)
+// space complexity: O(n)
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	var inPos = make(map[int]int, len(inorder))
+	for i, v := range inorder {
+		inPos[v] = i
+	}
+	return buildTreeHelper(preorder, 0, len(preorder)-1, 0, inPos)
+}
+
+func buildTreeHelper(preorder []int, preStart, preEnd int,
+	inStart int, inPos map[int]int) *TreeNode {
+	if preStart > preEnd {
+		return nil
+	}
+
+	var root = &TreeNode{Val: preorder[preStart]}
+	var rootIdx = inPos[preorder[preStart]]
+	var leftLen = rootIdx - inStart
+	root.Left = buildTreeHelper(preorder, preStart+1, preStart+leftLen, inStart, inPos)
+	root.Right = buildTreeHelper(preorder, preStart+leftLen+1, preEnd, rootIdx+1, inPos)
+	return root
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
