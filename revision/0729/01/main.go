2ff3000a7ddbdfd73543ff009ca9c8cb2387961b
mcrssuper <mcrwayfun@gmail.com>
2022-07-29 09:50:49 +0800

    feat: 106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ revision/0729/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+// time complexity: O(n)
+// space complexity: O(n)
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	var inPos = make(map[int]int, len(inorder))
+	for i, v := range inorder {
+		inPos[v] = i
+	}
+	return buildTreeHelper(postorder, 0, len(postorder)-1, 0, inPos)
+}
+
+func buildTreeHelper(postorder []int, postStart, postEnd int,
+	inStart int, inPos map[int]int) *TreeNode {
+	if postStart > postEnd {
+		return nil
+	}
+
+	var root = &TreeNode{Val: postorder[postEnd]}
+	var rootIdx = inPos[postorder[postEnd]]
+	var leftLen = rootIdx - inStart // 中序遍历中左子树的个数
+	root.Left = buildTreeHelper(postorder, postStart, postStart+leftLen-1, inStart, inPos)
+	root.Right = buildTreeHelper(postorder, postStart+leftLen, postEnd-1, rootIdx+1, inPos)
+	return root
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
