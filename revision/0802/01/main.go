dd2bd3f1e797b48d89484cc534d28a41ac7d31af
mcrssuper <mcrwayfun@gmail.com>
2022-08-02 21:41:23 +0800

    feat: 213. House Robber II

---
+++ revision/0802/01/main.go
@@ -0,0 +1,34 @@
+package main
+
+// time complexity: O(n)
+// space complexity: O(1)
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	var n = len(nums)
+	return max(helper(nums, 0, n-2), helper(nums, 1, n-1))
+}
+
+func helper(nums []int, start, end int) int {
+	var prev2 int
+	var prev1 int
+	for i := start; i <= end; i++ {
+		cur := max(prev1, prev2+nums[start])
+		prev2 = prev1
+		prev1 = cur
+	}
+	return prev1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
