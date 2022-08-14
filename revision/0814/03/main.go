6b080dc27dfaafdd33c7652e5569e12cf7de37dd
mcrssuper <mcrwayfun@gmail.com>
2022-08-14 16:14:34 +0800

    feat: 98. Validate Binary Search Tree

---
+++ revision/0814/03/main.go
@@ -0,0 +1,30 @@
+package main
+
+// time complexity: O(n)
+// space complexity: O(n)
+func isValidBST(root *TreeNode) bool {
+	return isValidBSTBound(root, nil, nil)
+}
+
+func isValidBSTBound(root *TreeNode, lower, upper *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	if lower != nil && root.Val <= lower.Val {
+		return false
+	}
+
+	if upper != nil && root.Val >= upper.Val {
+		return false
+	}
+
+	return isValidBSTBound(root.Left, lower, root) &&
+		isValidBSTBound(root.Right, root, upper)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
