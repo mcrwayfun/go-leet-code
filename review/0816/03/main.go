e7492b6786346191e3f3b5f39cc1cca555408097
mcrssuper <mcrwayfun@gmail.com>
2022-08-16 07:01:40 +0800

    feat: 62. Unique Paths

---
+++ review/0816/03/main.go
@@ -0,0 +1,29 @@
+package main
+
+// time complexity: O(n^2)
+// space complexity: O(n^2)
+func uniquePaths(m int, n int) int {
+	if m < 1 || n < 1 {
+		return 0
+	}
+
+	var d = make([][]int, m)
+	for i := range d {
+		d[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		d[i][0] = 1
+	}
+	for j := 0; j < n; j++ {
+		d[0][j] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			d[i][j] = d[i-1][j] + d[i][j-1]
+		}
+	}
+
+	return d[m-1][n-1]
+}
