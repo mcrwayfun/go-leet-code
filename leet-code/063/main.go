b0971540a535718c88974025d914bd906bf56bb5
mcrssuper <mcrwayfun@gmail.com>
2022-08-16 21:31:58 +0800

    feat: 63. Unique Paths II

--- leet-code/063/main.go
+++ leet-code/063/main.go
@@ -1,69 +1,60 @@
 package main
 
-import "fmt"
+/**
+一个机器人位于一个m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+示例 1：
+输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+输出：2
+解释：3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+示例 2：
+
+输入：obstacleGrid = [[0,1],[0,0]]
+输出：1
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/unique-paths-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {}
+*/
 
 // time complexity: O(n^2)
-// space complexity: O(n^2)
+// space complexity: O(n)
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	if len(obstacleGrid) == 0 {
+	if len(obstacleGrid) == 0 || len(obstacleGrid[0]) == 0 {
 		return 0
 	}
 
-	if len(obstacleGrid[0]) == 0 {
-		return 0
+	var m = len(obstacleGrid)
+	var n = len(obstacleGrid[0])
+	var d = make([]int, n)
+	if obstacleGrid[0][0] == 1 {
+		d[0] = 0
+	} else {
+		d[0] = 1
 	}
-
-	m := len(obstacleGrid)
-	n := len(obstacleGrid[0])
-	fn := initFn(m, n)
 
 	for i := 0; i < m; i++ {
-		if obstacleGrid[i][0] != 1 {
-			fn[i][0] = 1
-		} else {
-			break
+		if obstacleGrid[i][0] == 1 {
+			d[0] = 0
 		}
-	}
-
-	for i := 0; i < n; i++ {
-		if obstacleGrid[0][i] != 1 {
-			fn[0][i] = 1
-		} else {
-			break
-		}
-	}
-
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			if obstacleGrid[i][j] != 1 {
-				fn[i][j] = fn[i-1][j] + fn[i][j-1]
+		for j := 0; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				d[j] = 0
 			} else {
-				fn[i][j] = 0
+				d[j] = d[j] + d[j-1]
 			}
 		}
 	}
 
-	return fn[m-1][n-1]
-}
-
-func initFn(m, n int) [][]int {
-	fn := make([][]int, m)
-	for i := range fn {
-		fn[i] = make([]int, n)
-	}
-	return fn
-}
-
-func main() {
-	nums := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
-	pathSum := uniquePathsWithObstacles(nums)
-	fmt.Println(pathSum)
-
-	nums = [][]int{{1, 0}}
-	pathSum = uniquePathsWithObstacles(nums)
-	fmt.Println(pathSum)
-
-	nums = [][]int{{0, 0}, {1, 1}, {0, 0}}
-	pathSum = uniquePathsWithObstacles(nums)
-	fmt.Println(pathSum)
+	return d[n-1]
 }
