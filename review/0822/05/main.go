3c4a9aaac13cc6341a2ea8e81e8121c45ab39194
mcrssuper <mcrwayfun@gmail.com>
2022-08-22 22:11:36 +0800

    feat: 50. Pow(x, n)

---
+++ review/0822/05/main.go
@@ -0,0 +1,24 @@
+package main
+
+// time complexity: O(m)
+// space complexity: O(1)
+func myPow(x float64, n int) float64 {
+	var N = n
+	if n < 0 {
+		N = -N
+	}
+
+	var res float64 = 1
+	for N != 0 {
+		if N & 1 == 1 {
+			res *= x
+		}
+		x *= x
+		N <<= 1
+	}
+
+	if n < 0 {
+		return 1/res
+	}
+	return res
+}
