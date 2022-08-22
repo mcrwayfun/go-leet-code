7f2177ae17e52e2d20f8ee63943e3b4a62e9f4a9
mcrssuper <mcrwayfun@gmail.com>
2022-08-22 22:19:43 +0800

    feat: 2. Add Two Numbers

---
+++ review/0822/06/main.go
@@ -0,0 +1,35 @@
+package main
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	var carry int
+	var prev = &ListNode{}
+	var res = prev
+	for l1 != nil || l2 != nil || carry != 0 {
+		var sum = carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		res.Next = &ListNode{Val: sum%10}
+		res = res.Next
+		carry = sum / 10
+	}
+	return res
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
