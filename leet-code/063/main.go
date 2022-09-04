0425928af053b1409dc624b66872e62f8e0ac630
mcrssuper <mcrwayfun@gmail.com>
2022-09-04 21:16:59 +0800

    feat: 63. Unique Paths II

--- leet-code/063/main.go
+++ leet-code/063/main.go
@@ -58,3 +58,44 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 
 	return d[n-1]
 }
+
+// time complexity: O(n^2)
+// space complexity: O(n^2)
+func uniquePathsWithObstacles2(obstacleGrid [][]int) int {
+	if len(obstacleGrid) == 0 || len(obstacleGrid[0]) == 0 {
+		return 0
+	}
+
+	var m = len(obstacleGrid)
+	var n = len(obstacleGrid[0])
+	var d = make([][]int, m)
+	for i := range d {
+		d[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		if obstacleGrid[i][0] == 1 {
+			break
+		}
+		d[i][0] = 1
+	}
+
+	for j := 0; j < n; j++ {
+		if obstacleGrid[0][j] == 1 {
+			break
+		}
+		d[0][j] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				d[i][j] = 0
+			} else {
+				d[i][j] = d[i-1][j] + d[i][j-1]
+			}
+		}
+	}
+
+	return d[m-1][n-1]
+}
---
+++ review/0904/03/main.go
@@ -0,0 +1,34 @@
+package main
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if len(obstacleGrid) == 0 || len(obstacleGrid[0]) == 0 {
+		return 0
+	}
+
+	var m = len(obstacleGrid)
+	var n = len(obstacleGrid[0])
+	var d = make([]int, n)
+	if obstacleGrid[0][0] == 1 {
+		d[0] = 0
+	} else {
+		d[0] = 1
+	}
+
+	for i := 0; i < m; i++ {
+		if obstacleGrid[0][i] == 1 {
+			d[0] = 0
+		} else {
+			d[0] = 1
+		}
+
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				d[j] = 0
+			} else {
+				d[j] = d[j] + d[j-1]
+			}
+		}
+	}
+
+	return d[n-1]
+}
