960b83e2d7fc43d3569274e293951fbe0a2011a8
mcrssuper <mcrwayfun@gmail.com>
2022-11-18 23:56:31 +0800

    feat: 112. Path Sum

---
+++ review/1118/03/main.go
@@ -0,0 +1,23 @@
+package main
+
+// time complexity: O(n)
+// space complexity: O(n)
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil &&
+		root.Val == targetSum {
+		return true
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) ||
+		hasPathSum(root.Right, targetSum-root.Val)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
